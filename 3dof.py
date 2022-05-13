a7d56fd6723ebbe5dddf9504f67cfd73a5e4b92c
nguyenhau442001 <105471622+nguyenhau442001@users.noreply.github.com>
2022-05-13 21:50:06 +0700

    Update 3dof.py

--- 3dof.py
+++ 3dof.py
@@ -1,39 +1,31 @@
 import keras
-from keras.datasets import boston_housing
+import numpy as np
+import pandas as pd
 from tensorflow.keras.optimizers import RMSprop 
 from keras.callbacks import EarlyStopping  
 from sklearn import preprocessing
-from sklearn.preprocessing import scale, StandardScaler
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
+#Upload data to google.colab
 from google.colab import files 
 uploaded=files.upload()
 
-import pandas as pd
+
 url ='3dof.csv'
 dataframe=pd.read_csv(url)
 
 print(dataframe.shape)
 
-from sklearn.model_selection import train_test_split
 theta=dataframe.drop(['px','py','phi'], axis=1)
 position=dataframe.drop(['theta1','theta2','theta3'], axis=1)
 
 
 theta_train,theta_test,position_train,position_test=train_test_split(theta,position,test_size=0.2)
-print(theta_train.shape)  
-print(position_train.shape)
-
-print(theta_test.shape)  
-print(position_test.shape)
-
-
 
 theta=theta.astype('float32')
-
-
 model = Sequential()
 model.add(Dense(64, kernel_initializer='normal', activation='relu', input_shape=(3,)))
 model.add(Dense(64, activation='relu')) 
@@ -47,7 +39,8 @@ history=model.fit(theta,position,batch_size=256, epochs=1000, verbose=1, validat
 score = model.evaluate(theta,position, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-#verbose is the choice that how you want to see the output of your Neural Network while it's training. If you set verbose = 0, It will show nothing.
+
+#Plot
 ylim=(0,1)
 plt.plot(history.history['accuracy'])
 plt.xlabel('epoch')
@@ -55,7 +48,7 @@ plt.xlabel('epoch')
 plt.legend(['accuracy'])
 plt.show()                                                                                
 
-import numpy as np
+#Predict
 theta_test=np.array(theta_test)
 print(theta_test[700])
 pos_predict = model.predict(theta_test[700].reshape(1,3))
